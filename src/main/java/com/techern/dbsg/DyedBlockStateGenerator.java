ad78ad7ed38dd9e9e06bdc008b5699d311f7b3ae
Cruz Julian Bishop <cruzjbishop@gmail.com>
2016-04-12 10:50:15 +1000

    Get it running (Not really running, just complaining)

---
+++ src/main/java/com/techern/dbsg/DyedBlockStateGenerator.java
@@ -0,0 +1,20 @@
+package com.techern.dbsg;
+
+/**
+ * DyedBlockStateGenerator; A block state generator for dyed blocks
+ *
+ * @since 0.0.1
+ */
+public class DyedBlockStateGenerator {
+
+    /**
+     * Runs {@link DyedBlockStateGenerator}
+     *
+     * @param arguments The list of arguments
+     * @since 0.0.1
+     */
+    public static void main(String... arguments) {
+        System.out.println("Go away, I'm not ready to serve you yet");
+    }
+
+}
